baae40b07dd911015dac7fb1d559372296adcd8d
root <CaramonH>
2023-05-09 13:53:38 -0700

    hewwo

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-print(f"{chr(97)}", end="")
-for i in range(1, 26):
-    print(f"{chr(97+i)}", end="")
+import string
+for i in range(0, 26):
+    print("{}".format(string.ascii_lowercase[i]), end="")
