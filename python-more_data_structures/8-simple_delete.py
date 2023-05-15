4ed75622f65a558260b86c1d8b4a2b6fb771d3ab
root <Caramonh@yahoo.com>
2023-05-15 12:54:58 -0700

    8 pep8 fix

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -2,7 +2,7 @@
 # Write a function that deletes a key in a dictionary.
 
 
-def simple_delete(a_dictionary, key =""):
+def simple_delete(a_dictionary, key=""):
     if key in a_dictionary:
         del a_dictionary[key]
     return a_dictionary
