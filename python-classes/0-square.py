fe546a63ae22d0b64d6700fcdd7888c30f99da7b
root <Caramonh@yahoo.com>
2023-05-18 09:26:46 -0700

    square

---
+++ python-classes/0-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Write an empty class Square that defines a square:"""
+
+
+class Square:
+    """This class defines a square"""
+    def __init__(self, size=None):
+        """This initializes a square with a size"""
+        self.__size = size
