bd8706fba4c6f610221b83f4bbbe818901d4db78
root <Caramonh@yahoo.com>
2023-05-18 09:54:12 -0700

    ez pz

---
+++ python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square"""
+
+
+class Square:
+    """this class defines Square"""
+    def __init__(self, size=0):
+        """this initializes a simple square"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >=0")
+        self.__size = size
