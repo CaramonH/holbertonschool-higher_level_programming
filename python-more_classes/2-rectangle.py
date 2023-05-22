152d2a34007e3a0d1377a655a9e957c1e1a400f7
root <Caramonh@yahoo.com>
2023-05-22 12:31:30 -0700

    ststter

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle:
     def width(self):
         return self.__width
 
-    @width.sestter
+    @width.setter
     def width(self, value):
         if type(value) is not int:
             raise TypeError("width must be an integer")
