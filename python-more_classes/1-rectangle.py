b4f8deb6952e6d5946a7a012e83be18dc728e7a8
root <Caramonh@yahoo.com>
2023-05-22 12:05:32 -0700

    underdawg

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -7,11 +7,7 @@ A class Rectangle that defines a rectangle by:
 
 class Rectangle:
     """Rectangle class"""
-
     def __init__(self, width=0, height=0):
-        """
-
-        """
         self.height = height
         self.width = width
 
@@ -21,9 +17,6 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        """
-
-        """
         if type(value) is not int:
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -33,16 +26,10 @@ class Rectangle:
 
     @property
     def height(self):
-        """
-
-        """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """
-
-        """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         elif value < 0:
