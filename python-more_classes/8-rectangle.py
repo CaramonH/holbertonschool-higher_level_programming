f7bcda42491a2777f85869e38cf15f3b667be695
root <Caramonh@yahoo.com>
2023-05-22 16:27:35 -0700

    hm

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -82,3 +82,7 @@ class Rectangle:
     def __del__(self):
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
+
+    @classmethod
+    def square(cls, size=0):
+        return cls(size, size)
