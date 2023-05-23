724b66ad4529c445161c7cc72a3db7b7533ac1cd
root <Caramonh@yahoo.com>
2023-05-23 10:47:28 -0700

    idk

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -46,3 +46,13 @@ class TestMaxInteger(unittest.TestCase):
     def test_empty_list(self):
         test_list = []
         self.assertEqual(max_integer(test_list), None)
+
+    def max_integer(list=[]):
+        if len(list) == 0:
+            return None
+        result = list[0]
+        i = 1
+        while i < len(list):
+            if list[i] > result:
+                result = list[i]
+            i += 1                                                                    return result
