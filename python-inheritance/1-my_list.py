9afb69c12dcf390485854d693c0e4c19ce32db72
root <Caramonh@yahoo.com>
2023-05-26 09:18:25 -0700

    1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""
-A module that prints a list in ascending order
-"""
+"""A module that prints a list in ascending order"""
 
 
 class MyList(list):
+    """Implements sorted printing for list class"""
+
     def print_sorted(self):
         if issubclass(MyList, list):
             print(sorted(self))
