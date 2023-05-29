b562b682c12e29fd9ef0e1819e3e74b30085ddba
root <Caramonh@yahoo.com>
2023-05-29 12:04:52 -0700

    malk

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" 6-load_from_json_file Module """
+import json
+
+
+def load_from_json_file(filename):
+    """ Function that creates a Object from a JSON file."""
+    with open(filename, mode='r', encoding='utf-8') as f:
+        return json.load(f)
