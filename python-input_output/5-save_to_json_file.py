b2f8ef6b2a7f54c4f53658462f9e41821e560ac0
root <Caramonh@yahoo.com>
2023-05-29 11:52:19 -0700

    zelda

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" 5-save_to_json_file Module """
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Write an object to a text file using JSON."""
+    with open(filename, "w") as f:
+        json.dumps(my_obj, f)
