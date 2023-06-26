477658ee532b6bff9b5148723bb868ea7716dc5b
root <Caramonh@yahoo.com>
2023-06-26 10:27:44 -0700

    9

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,13 @@
+-- Comment
+CREATE table IF NOT EXISTS second_table (
+	is INT,
+	name VARCHAR(256),
+	score INT
+);
+
+INSERT INTO second_table (id, name, score)
+VALUES
+    (1, "John", 10),
+    (2, "Alex", 3),
+    (3,"Bob", 14),
+    (4, "George", 8);
