0eae6d1a4bbf7059d66124985efb74c24013d66f
root <Caramonh@yahoo.com>
2023-07-03 12:02:54 -0700

    module now documented

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,6 +7,7 @@ import MySQLdb
 
 
 def print_state():
+    """List the states"""
     from sys import argv
     db = MySQLdb.connect(host="localhost", port 3306, user=argv[1],
                          passwd=argv[2], database=argv[3])
