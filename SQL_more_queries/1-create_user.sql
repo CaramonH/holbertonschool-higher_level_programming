023be5c225ac8603aa56576cda5c27d712f16347
root <Caramonh@yahoo.com>
2023-07-03 15:15:43 -0700

    it was worth a shot

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,12 +1,3 @@
--- Check if user_0d_1 already exists
-SELECT EXISTS(SELECT 1 FROM mysql.user WHERE user = 'user_0d_1') INTO @user_exists;
-
--- Create user_0d_1 if it doesn't exist
-IF @user_exists = 0 THEN
-    CREATE USER 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
-    GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-    FLUSH PRIVILEGES;
-    SELECT 'User user_0d_1 created with all privileges.';
-ELSE
-    SELECT 'User user_0d_1 already exists. Skipping user creation.';
-END IF;
+-- Write a script that creates the MySQL server user user_0d_1
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
