81664814f490708ea9eb743c5ad5e482c4523a18
root <Caramonh@yahoo.com>
2023-07-03 18:58:25 -0700

    newline

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -15,4 +15,3 @@ class City(Base):
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'),
                       autoincrement=True, nullable=False)
-    
