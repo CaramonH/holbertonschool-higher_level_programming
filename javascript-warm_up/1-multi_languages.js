f25f684313c390b19f8ad42af372953d9e56f60b
Caramonh <Caramonh@yahoo.com>
2023-07-31 09:58:44 -0700

    4

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+let theString = 'C is fun';
+console.log(theString);
+theString = 'Python is cool';
+console.log(theString);
+theString = 'JavaScript is amazing';
+console.log(theString);
